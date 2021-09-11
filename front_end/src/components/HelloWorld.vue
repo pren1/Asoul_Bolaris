ae6467fe55f140e261add94607001fbf28ad38e3
PengRen <pren1@ucsc.edu>
2021-09-10 23:43:47 -0700

    ok!

--- front_end/src/components/HelloWorld.vue
+++ front_end/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <h2 class="title">{{ title }}</h2>
-    <v-chart class="chart" :option="option" theme="light"/>
+    <v-chart ref="pie" class="chart" :option="option" autoresize theme="light"/>
   </div>
 </template>
 
@@ -25,7 +25,9 @@ use([
   TooltipComponent,
   LegendComponent
 ]);
-
+let dataIndex = -1;
+let new_dataIndex = -1; // record the new position
+let delay_time = 1000; // specific delay
 export default {
   mixins: [mixin],
   name: "HelloWorld",
@@ -39,10 +41,12 @@ export default {
     title: 'HelloWorld',
     option: {},
     money_pie: [],
-    type_pie: []
+    type_pie: [],
+    stopped: false,
+    timeout: null
   }),
   created() {
-    // this.update_chart()
+    this.update_chart()
     this.url = `https://asoulmonitor.xyz/api/data/2021_9_10_22634198_dm_pie_picture.json`
   },
   watch: {
@@ -56,25 +60,53 @@ export default {
   },
   methods: {
     update_chart() {
-      this.title = "New title"
+      this.title = "单次礼物价格统计"
       this.option = {
         tooltip: {
           trigger: "item",
           formatter(data) {
-            console.log(data.data.name)
-            return data.data.name.split(":")[1]
+            if (data.dataIndex !== dataIndex){
+              new_dataIndex = data.dataIndex; // record the mouse position
+              delay_time = 2000; // if the mouse put in the pie chart, change delay here
+            }
+            // console.log(data.data.name)
+            return data.data.name
           }
         },
-        series: [
+        series:
+        //     [
+        //   {
+        //     name: "Traffic Sources",
+        //     type: "pie",
+        //     radius: "55%",
+        //     center: ["50%", "50%"],
+        //     data: [
+        //       { value: Math.round(this.money_pie[0][1]), name: this.money_pie[0][0] },
+        //       { value: Math.round(this.money_pie[1][1]), name: this.money_pie[1][0] },
+        //       { value: Math.round(this.money_pie[2][1]), name: this.money_pie[2][0] }
+        //     ],
+        //     minShowLabelAngle: 3,
+        //     emphasis: {
+        //       itemStyle: {
+        //         shadowBlur: 10,
+        //         shadowOffsetX: 0,
+        //         shadowColor: "rgba(0, 0, 0, 0.5)"
+        //       }
+        //     }
+        //   }
+        // ]
+        [
           {
             name: "Traffic Sources",
             type: "pie",
             radius: "55%",
             center: ["50%", "50%"],
             data: [
-              { value: Math.round(this.money_pie[0][1]), name: this.money_pie[0][0] },
-              { value: Math.round(this.money_pie[1][1]), name: this.money_pie[1][0] },
-              { value: Math.round(this.money_pie[2][1]), name: this.money_pie[2][0] }
+              { value: 335, name: "Direct" },
+              { value: 310, name: "Email" },
+              { value: 234, name: "Ad Networks" },
+              { value: 135, name: "Video Ads" },
+              { value: 1548, name: "Search Engines" }
             ],
             emphasis: {
               itemStyle: {
@@ -86,8 +118,59 @@ export default {
           }
         ]
       }
+    },
+    highlight() {
+      try {
+        const pie = this.$refs.pie;
+        // console.log(this.option.series[0].data)
+        if (typeof this.option.series != "undefined") {
+          const dataLen = this.option.series[0].data.length;
+          pie.dispatchAction({
+            type: 'downplay',
+            seriesIndex: 0,
+            dataIndex
+          });
+          if (delay_time===1000){ // If the delay is sometime else, do not highlight current dataIndex
+            dataIndex = (dataIndex + 1) % dataLen;
+            pie.dispatchAction({
+            type: 'highlight',
+            seriesIndex: 0,
+            dataIndex
+            });
+            // 显示 tooltip
+            pie.dispatchAction({
+              type: 'showTip',
+              seriesIndex: 0,
+              dataIndex,
+              position: 'inside'
+            });
+          }
+        }
+        if (this.stopped) return;
+        this.timeout = setTimeout(this.highlight, delay_time);
+          if(delay_time !== 1000){ // Only when the delay time is not 1000, renew the dataIndex, begin at the new data Index
+            dataIndex = new_dataIndex;
+            delay_time = 1000; // recover the delay_time
+          }
+        }
+        catch (error) {
+          console.error(error);
+          if (this.stopped) return;
+          this.timeout = setTimeout(this.highlight, delay_time);
+          if(delay_time !== 1000){
+              dataIndex = new_dataIndex;
+              delay_time = 1000;
+            }
+      }
     }
-  }
+  },
+  mounted () {
+    this.highlight()
+  },
+  beforeDestroy() {
+    this.stopped = true;
+    clearTimeout(this.timeout)
+  },
 
   // data() {
   //   return {
