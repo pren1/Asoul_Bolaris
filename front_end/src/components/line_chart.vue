ff8c6401088712e617df81d6a864590891b6b647
PengRen <pren1@ucsc.edu>
2021-09-11 17:06:44 -0700

    hold+

--- front_end/src/components/line_chart.vue
+++ front_end/src/components/line_chart.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card">
-    <h2 class="title">{{ title }}</h2>
+<!--    <h2 class="title">{{ title }}</h2>-->
     <v-chart class="chart" :option="option" autoresize :theme="input_theme"/>
   </div>
 </template>
@@ -45,9 +45,9 @@ export default {
       this.update_chart()
     }
   },
-  // created() {
-  //   this.update_chart()
-  // },
+  created() {
+    this.update_chart()
+  },
   methods: {
     format_data(value){
       var result = []
@@ -60,58 +60,76 @@ export default {
     update_chart() {
       this.title = this.input_title
       if (this.calculate_total === "计算总和") {
-        this.title += "合计：" + this.data_array_1d.reduce((x, y) => x + y)
+        // this.title += "合计：" + this.data_array_1d.reduce((x, y) => x + y)
       }
       this.option = {
-          xAxis: {
-            // type: 'category',
-            // // boundaryGap: false,
-            // // data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-            // data: [0,1 ,2 ,3, 4, 5, 6]
-            type: 'value',
-            interval:10, // 步长
-            min: -5,
-            max: this.line_data[this.line_data.length-1][0],
-            splitLine:{
-              show:false
-            },
-            name: this.input_x_label,
-            nameLocation: 'middle',
-            nameGap: 30
-          },
-          yAxis: {
-             type: 'value',
-             axisLine:{
-              show:false
-             },
-              name: this.input_y_label,
-              nameLocation: 'middle',
-              nameGap: 40
-          },
-         tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-                type: 'cross',
-                // label: {
-                //     backgroundColor: '#6a7985'
-                // }
-            },
-            formatter(data) {
-              if (data){
-                // console.log(data[0].data)
-                // var colorSpan = color => '<span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:' + color + '"></span>';
-                return '时间：' + data[0].data[0] + '<br>' + '数值：' + data[0].data[1] + '</br>'
-              }
-              return null
-            }
-          },
-        series: [{
-          data: this.line_data,
-          type: 'line',
-          smooth: true,
-          showSymbol: false,
-          areaStyle: {}
-        }]
+        title: {
+        left: 'center',
+        top: '4%',
+        text: '大数据量面积图',
+    },
+        xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        areaStyle: {}
+    }]
+        //   xAxis: {
+        //     // type: 'category',
+        //     // // boundaryGap: false,
+        //     // // data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        //     // data: [0,1 ,2 ,3, 4, 5, 6]
+        //     type: 'value',
+        //     interval:10, // 步长
+        //     min: -5,
+        //     max: this.line_data[this.line_data.length-1][0],
+        //     splitLine:{
+        //       show:false
+        //     },
+        //     name: this.input_x_label,
+        //     nameLocation: 'middle',
+        //     nameGap: 30
+        //   },
+        //   yAxis: {
+        //      type: 'value',
+        //      axisLine:{
+        //       show:false
+        //      },
+        //       name: this.input_y_label,
+        //       nameLocation: 'middle',
+        //       nameGap: 40
+        //   },
+        //  tooltip: {
+        //     trigger: 'axis',
+        //     axisPointer: {
+        //         type: 'cross',
+        //         // label: {
+        //         //     backgroundColor: '#6a7985'
+        //         // }
+        //     },
+        //     formatter(data) {
+        //       if (data){
+        //         // console.log(data[0].data)
+        //         // var colorSpan = color => '<span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:' + color + '"></span>';
+        //         return '时间：' + data[0].data[0] + '<br>' + '数值：' + data[0].data[1] + '</br>'
+        //       }
+        //       return null
+        //     }
+        //   },
+        // series: [{
+        //   data: this.line_data,
+        //   type: 'line',
+        //   smooth: true,
+        //   showSymbol: false,
+        //   areaStyle: {}
+        // }]
       }
     }
   }
